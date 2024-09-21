38bb6baf44742a6f6e0820249f4dbf8ccbff2da1
Bala Ganaparthi <bala.ganaparthi@okta.com>
2024-09-21 00:34:22 -0500

    first commit

--- netlify/functions/showpage.js
+++ netlify/functions/showpage.js
@@ -1,36 +1,22 @@
+const express = require("express");
 const nunjucks = require("nunjucks");
-const path = require("path");
+const serverless = require("serverless-http");
 
-const viewpath = path.join(__dirname, "../views");
+const app = express();
 
-nunjucks.configure(viewpath, {
+// Configure Nunjucks
+nunjucks.configure("views", {
   autoescape: true,
   throwOnUndefined: true,
 });
-// app.set("views", viewpath);
-// app.set("view engine", "html");
 
-exports.handler = async (event, context) => {
-  try {
-    // Render the Nunjucks template
-    const html = nunjucks.render("patient_data.html", {
-      // You can pass data to the template here if needed
-      title: "Patient Data",
-      // Add other context variables as necessary
-    });
+// Define an endpoint
+app.get("/showpage", (req, res) => {
+  const html = nunjucks.render("patient_data.html", {
+    title: "Patient Data",
+  });
+  res.send(html);
+});
 
-    return {
-      statusCode: 200,
-      headers: {
-        "Content-Type": "text/html",
-      },
-      body: html,
-    };
-  } catch (error) {
-    console.error("Error rendering template:", error);
-    return {
-      statusCode: 500,
-      body: "Internal Server Error",
-    };
-  }
-};
+// Export as a serverless function
+module.exports.handler = serverless(app);
